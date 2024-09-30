610919f42d4df5d965e3f0af70278e94540920a6
Keerthanbingi <saikeerthanbingi@gmail.com>
2024-09-29 23:17:37 -0400

    react json file

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    
+
+    <footer className="bottom">
+      <div className="center">
+        
+        <p>Changed by keerthan</p>
+      </div>
+    </footer>
+  );
+  
+}
+
+export default Footer;
